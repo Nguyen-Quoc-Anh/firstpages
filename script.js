af57fc8a5a791ff9ffed6c05ed6f5c865e6c29c7
Nguyen-Quoc-Anh <quocanhseu@gmail.com>
2021-09-18 13:35:14 +0700

    upload

---
+++ script.js
@@ -0,0 +1,32 @@
+window.addEventListener("load", function(){
+    const slideCount = document.querySelectorAll("#slider-wrapper ul li").length;
+    const slideWidth = document.querySelector("#slider-wrapper").offsetWidth;
+    const totalWidth = (slideCount * slideWidth) + "px";
+    const slider = document.querySelector("#slider-wrapper ul");
+    const next = document.getElementById("next");
+    const previous = document.getElementById("prev");
+
+    let leftPosition = 0;
+    let counter = 0;
+    slider.style.width = totalWidth;
+
+    next.addEventListener("click", function(event){
+        event.preventDefault();
+        counter++;
+        if(counter == slideCount){
+            counter = 0;
+        }
+        leftPosition = `-${counter * slideWidth}px`;
+        slider.style.left = leftPosition;
+    });
+
+    previous.addEventListener("click", function(event){
+        event.preventDefault();
+        counter--;
+        if(counter < 0){
+            counter = slideCount - 1;
+        }
+        leftPosition = `-${counter * slideWidth}px`;
+        slider.style.left = leftPosition;
+    });
+});
